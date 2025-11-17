f5f419eaf52a43b10f96ab92db2f19e2721979a0
Eugene Park <eugenepark2001@gmail.com>
2025-11-17 19:14:46 +0100

    Enable audio for nixos

--- hosts/personal/thinkpad-p16s/home.nix
+++ hosts/personal/thinkpad-p16s/home.nix
@@ -117,6 +117,15 @@
 			"$mod, mouse:272, movewindow"
 			"$mod, mouse:273, resizewindow"
 		];
+		bindel = [
+			# Laptop multimedia keys for volume and LCD brightness
+			",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+			",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+			",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+			",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+			",XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+"
+			",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
+		];
 		bind = [
 			"$mod, F, exec, firefox"
 			"$mod, Return, exec, kitty"
