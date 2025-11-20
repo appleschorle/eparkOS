f0d947c1021fc7d0934d0312cee259c524afba53
Eugene Park <eugenepark2001@gmail.com>
2025-11-20 14:58:10 +0100

    Add nixvim config

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -1,21 +1,99 @@
-{ inputs, config, lib, ... }:
-
-with lib;
 {
-	imports = [
-		inputs.nixvim.homeModules.nixvim
-	];
+  pkgs,
+  inputs,
+  config,
+  lib,
+  ...
+}:
+with lib; {
+  imports = [
+    inputs.nixvim.homeModules.nixvim
+  ];
 
-	options.myNvim.enable = mkEnableOption "Enable NeoVim";
+  options.myNvim.enable = mkEnableOption "Enable NeoVim";
 
-	config = mkIf config.myNvim.enable {
-		programs.nixvim = {
-			enable = true;
-			defaultEditor = true;
-			viAlias = true;
-			vimAlias = true;
-			colorschemes.gruvbox-material-nvim.enable = true;
-			plugins.lualine.enable = true;
-		};	
-	};
+  config = mkIf config.myNvim.enable {
+    home.packages = with pkgs; [
+      alejandra
+      # Add other formatters you might want here, e.g., stylua, black, prettier
+    ];
+    programs.nixvim = {
+      enable = true;
+
+      defaultEditor = true;
+
+      viAlias = true;
+      vimAlias = true;
+
+      dependencies = {
+        ripgrep.enable = true;
+        gcc.enable = true;
+        tree-sitter.enable = true;
+        nodejs.enable = true;
+      };
+
+      lsp.servers = {
+        "*" = {
+          config = {
+            capabilities = {
+              textDocument = {
+                semanticTokens = {
+                  multilineTokenSupport = true;
+                };
+              };
+            };
+            root_markers = [
+              ".git"
+            ];
+          };
+        };
+        clangd = {
+          config = {
+            cmd = [
+              "clangd"
+              "--background-index"
+            ];
+            filetypes = [
+              "c"
+              "cpp"
+            ];
+            root_markers = [
+              "compile_commands.json"
+              "compile_flags.txt"
+            ];
+          };
+          enable = true;
+        };
+        nixd = {
+          enable = true;
+          config = {
+            cmd = ["nixd"];
+            filetypes = ["nix"];
+            root_markers = ["flake.nix" "flake.json"];
+          };
+        };
+        lua_ls = {
+          enable = true;
+        };
+      };
+      colorschemes.gruvbox-material-nvim.enable = true;
+      plugins.treesitter = {
+        enable = true;
+        settings = {
+          auto_install = true;
+        };
+      };
+      plugins.lualine.enable = true;
+      plugins.conform-nvim.enable = true;
+      plugins.conform-nvim.settings = {
+        formatters_by_ft = {
+          nix = ["alejandra"];
+        };
+        format_on_save = {
+          lsp_format = "fallback";
+          timeout_ms = 500;
+        };
+      };
+    };
+  };
 }
