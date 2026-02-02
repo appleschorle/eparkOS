2a7402457c2cd1fe9389ee518a808b18b8fb587c
Eugene Park <eugene.park@scania.com>
2026-02-02 08:29:31 +0100

    Create work setup

---
+++ hosts/work/macbook-pro-m4/home.nix
@@ -0,0 +1,40 @@
+{
+  flakeRootPath,
+  inputs,
+  ...
+}: let
+  homePath = "/home/epark";
+in {
+  imports = [
+    "${flakeRootPath}/modules/home-manager/nixvim"
+    "${flakeRootPath}/modules/home-manager/firefox.nix"
+    "${flakeRootPath}/modules/home-manager/tmux.nix"
+    "${flakeRootPath}/modules/home-manager/lazygit.nix"
+    "${flakeRootPath}/modules/home-manager/zoxide.nix"
+    "${flakeRootPath}/modules/home-manager/zsh.nix"
+    "${flakeRootPath}/modules/home-manager/kitty.nix"
+  ];
+
+  config = {
+    home = {
+      username = "epark";
+      homeDirectory = homePath;
+      stateVersion = "25.05";
+    };
+    programs.home-manager.enable = true;
+
+    nixpkgs.config.allowUnfree = true;
+
+    colorScheme = inputs.nix-colors.colorSchemes.gruvbox-dark-hard;
+
+    epark = {
+      nixvim.enable = true;
+      firefox.enable = true;
+      lazygit.enable = true;
+      tmux.enable = true;
+      zoxide.enable = true;
+      zsh.enable = true;
+      kitty.enable = true;
+    };
+  };
+}
