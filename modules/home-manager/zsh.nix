f8d6ff0df6a45c2a7e9e44546f5a1beec4965758
Eugene Park <eugene.park@scania.com>
2026-02-03 14:18:06 +0100

    Revert "add ruby path for mac"
    This reverts commit f9afc1fcade5a264ff04ab0df4a27f46dc9755cd.

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -16,7 +16,6 @@ in {
     home.sessionPath = [
       "/opt/homebrew/bin"
       "/opt/homebrew/sbin"
-      "/opt/homebrew/opt/ruby/bin/ruby"
       "/home/epark/.local/share/gem/ruby/3.3.0/bin"
     ];
 
