87e77d480ad78adbfb87dc37888b032d9e2bf703
Eugene Park <eugenepark2001@gmail.com>
2026-02-19 22:04:24 +0100

    Rice powermenu

--- modules/home-manager/hyprland/rofi/powermenu.nix
+++ modules/home-manager/hyprland/rofi/powermenu.nix
@@ -4,8 +4,21 @@
   pkgs,
   ...
 }: let
+  themeFileBackgroundImagePath = "rofi/themes/powermenu.jpg";
+  themeFilePath = "rofi/themes/powermenu.rasi";
+  themeFile = pkgs.replaceVars ./themes/powermenu.rasi {
+    base00 = config.colorscheme.palette.base00;
+    base03 = config.colorscheme.palette.base03;
+    base05 = config.colorscheme.palette.base05;
+    # base08 = config.colorscheme.palette.base08;
+    base0B = config.colorscheme.palette.base0B;
+    base0D = config.colorscheme.palette.base0D;
+    backgroundImage = "/home/epark/.config/${themeFileBackgroundImagePath}";
+  };
+
   powerMenuScript = pkgs.replaceVars ./static/powermenu.sh {
     rofi = pkgs.rofi;
+    themeFile = "$HOME/.config/${themeFilePath}";
   };
   rofiPowerMenu = pkgs.writeShellScriptBin "rofi-powermenu" (builtins.readFile powerMenuScript);
 
@@ -17,5 +30,8 @@ in {
     home.packages = [
       rofiPowerMenu
     ];
+
+    xdg.configFile."${themeFilePath}".source = themeFile;
+    xdg.configFile."${themeFileBackgroundImagePath}".source = ./themes/powermenu.jpg;
   };
 }
--- modules/home-manager/hyprland/rofi/static/powermenu.sh
+++ modules/home-manager/hyprland/rofi/static/powermenu.sh
@@ -1,7 +1,11 @@
-options="Shutdown\nReboot\nLock"
+#!/bin/bash
+
+theme_file="@themeFile@"
+
+options=" \n󰜉 \n "
 
 # Use placeholders for Nix-provided paths
-chosen=$(echo -e "$options" | @rofi@/bin/rofi -dmenu -i -p "Power")
+chosen=$(echo -e "$options" | @rofi@/bin/rofi -dmenu -i -p " $USER@" -theme $theme_file)
 
 case $chosen in
 Shutdown) systemctl poweroff ;;
