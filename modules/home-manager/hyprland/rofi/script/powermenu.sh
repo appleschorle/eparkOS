9aeab628b2536b47c5b236107849e9d54661931b
Eugene Park <eugenepark2001@gmail.com>
2026-02-21 20:05:01 +0100

    Rice powermenu

--- modules/home-manager/hyprland/rofi/script/powermenu.sh
+++ modules/home-manager/hyprland/rofi/script/powermenu.sh
@@ -2,14 +2,17 @@
 
 theme_file_path="@themeFile@"
 
-options=" \n󰜉 \n "
+power=""
+reboot="󰜉"
+lock=""
+options=("$power" "$reboot" "$lock")
 last_login="$(last "$USER" | head -n1 | tr -s ' ' | awk '{printf "%s %02d, %s", $4,$5,$6}')"
 uptime="$(uptime | awk '{print $3}' | sed 's/.$//')"
-
-chosen=$(echo -e "$options" | @rofi@/bin/rofi -dmenu -p " $USER" -mesg " Last Login: $last_login |  Uptime: $uptime" -theme $theme_file_path)
+# -p " $USER"
+chosen=$(printf "%s\n" "${options[@]}" | @rofi@/bin/rofi -dmenu -p " $USER" -mesg " Last Login: $last_login |  Uptime: $uptime" -theme $theme_file_path)
 
 case $chosen in
-" ") systemctl poweroff ;;
-"󰜉 ") systemctl reboot ;;
-" ") hyprlock ;;
+"$power") systemctl poweroff ;;
+"$reboot") systemctl reboot ;;
+"$lock") hyprlock ;;
 esac
