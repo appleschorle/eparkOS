77777228b6233ad0a12ca3fb514e7495867d5240
Eugene Park <eugene.park@scania.com>
2026-04-01 12:56:16 +0200

    Add tangerine specifics

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -39,6 +39,11 @@ in {
       '';
       shellAliases = {
         ll = "ls -lh";
+        sc-list = ''aws servicecatalog search-provisioned-products --region eu-west-1 --query "ProvisionedProducts[*].{Name:Name,Status:Status,CreatedTime:CreatedTime}" --output table'';
+        ff-mk = ''aws servicecatalog provision-product --region eu-west-1 --product-name gov-team-account-fire-fighter-role --provisioning-artifact-id $(aws servicecatalog describe-product --name gov-team-account-fire-fighter-role --region eu-west-1 --query "ProvisioningArtifacts[0].Id" --output text) --provisioned-product-name gov-team-account-fire-fighter-role-$(date +"%s")'';
+        ff-rm = ''for id in $(aws servicecatalog search-provisioned-products --region eu-west-1 --query "ProvisionedProducts[*].{Name:Name,Id:Id,Status:Status,CreatedTime:CreatedTime}" --output json | jq -rc '.[] | select(.Name | contains ("gov-team-account-fire-fighter-role-")) | .Id'); do aws servicecatalog terminate-provisioned-product --region eu-west-1 --provisioned-product-id ''$id; done'';
+        seed-mk = ''aws servicecatalog provision-product --region eu-west-1 --product-name gov-team-account-federated-deploy-role --provisioning-artifact-id $(aws servicecatalog describe-product --name gov-team-account-federated-deploy-role --region eu-west-1 --query "ProvisioningArtifacts[0].Id" --output text) --provisioned-product-name gov-team-account-federated-deploy-role-seed-$(date +"%s") --provisioning-parameters Key=AuthorizedGitlabPath,Value="csdt-gitlab/gitlab-ha/gitlab-seed:ref_type:*:ref:*" Key=OidcProviderName,Value=gitlab.scania.com Key=Suffix,Value=seed'';
+        seed-rm = ''for id in $(aws servicecatalog search-provisioned-products --region eu-west-1 --query "ProvisionedProducts[*].{Name:Name,Id:Id,Status:Status,CreatedTime:CreatedTime}" --output json | jq -rc '.[] | select(.Name | contains ("gov-team-account-federated-deploy-role-seed-")) | .Id'); do aws servicecatalog terminate-provisioned-product --region eu-west-1 --provisioned-product-id ''$id; done'';
       };
       shellGlobalAliases = {
         G = "| grep";
