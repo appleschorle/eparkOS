7fd5d4f34f9bc9a8fffa32c85dd49d6df4d019ba
Eugene Park <eugenepark2001@gmail.com>
2026-09-02 10:56:27 +0200

    Remove dconf as it does not pair well with nixos

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -3,23 +3,6 @@
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
 
-  programs.dconf = {
-    enable = true;
-    profiles = {
-      user.databases = [
-        {
-          settings = {
-            "org/gnome/desktop/input-sources" = {
-              show-all-sources = true;
-              sources = "[('xkb', 'us'), ('xkb', 'se'), ('xkb', 'de')]";
-              xkb-options = [ "grp:win_space_toggle" ];
-            };
-          };
-        }
-      ];
-    };
-  };
-
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   boot.loader = {
